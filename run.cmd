c01970ff72cdbb53f5d8901147b51be53b583f11
fredrikorn <48676646+fredrikorn@users.noreply.github.com>
2021-06-28 00:55:49 +0200

    Adds \output if it doesn't exist

--- run.cmd
+++ run.cmd
@@ -1,5 +1,7 @@
 echo. && echo SETTING PYTHON PATH:
 set PYTHONPATH=c:\git\CameraTraps\CameraTraps;c:\git\ai4eutils
 
+if not exist "output" mkdir output
+
 python CameraTraps/detection/run_tf_detector_batch.py ./md_v4.1.0.pb images ./output/output.json
 python output_record.py
